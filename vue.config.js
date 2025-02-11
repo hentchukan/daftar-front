10754ae13917014979eee65d2e8cf4ddd9c57130
iori-yagami <ferchichi.farooq@gmail.com>
2025-02-12 00:13:00 +0100

    feat(daftar-front): fix github action

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
     configureWebpack: {
         devtool: 'source-map'
     },
-    publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
+    publicPath: '/',
 }
