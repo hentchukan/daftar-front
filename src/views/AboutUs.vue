12db612674309c92e979aa237505587ff71f6f42
iori-yagami <ferchichi.farooq@gmail.com>
2025-04-22 23:27:26 +0200

    feat(daftar-front): update the menu. MVP version

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -20,7 +20,7 @@
         <h1 class="daftar-title text-5xl md:text-8xl xl:text-8xl text-center sm:text-right whitespace-nowrap text-ternary-dark dark:text-primary-light uppercase">
           الـدّفتـــر الأزرق
         </h1>
-        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf leading-none text-gray-400">
+        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf text-gray-400">
           صدرت صفحة الدفتر الأزرق سنة 2021، بعد أكثر من عشر سنوات من الكتابة عن السّينما
           في الجرائد الإلكترونيّة والنّشريّاتِ الورقيّةِ ومجموعاتٍ مختصّةٍ على مواقعِ التواصلِ
           وغيرها.
@@ -30,12 +30,12 @@
           وبعضُها لا يملك سياسةً تحريريّة من أساسِه، بل لا يقدّم تحريرا من أي شكل.
           فكان الدفتر الأزرق على فايسبوك فضاءً لكتابةٍ حُرّة لا تلتزمُ إلاّ بشغفعها بالسينما.
         </p>
-        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf leading-none text-gray-400">
+        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf text-gray-400">
           ولم تقتصر الصفحة على المقالات النقديّة الطويلة، وإنّما نشرت مراجعات قصيرةً ومقترحاتٍ متنوّعةً خفيفةً لم تلبث أن طغت،
           فانحسرت الكتابات الطويلةُ رضوخا لسياسات مواقع التواصل المُغلِّبةِ لمنطق المنشوراتِ البرقيّةِ وثقافة الرّيلز.
           وكان من الضروريِّ البحثُ عن فضاءاتِ نشرٍ أخرى أكثرَ ملاءمةً. يجب أن يلائم الفضاءُ المحتوى لا العكسُ.
         </p>
-        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf leading-none text-gray-400">
+        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf text-gray-400">
           جاء موقع الدفتر الأزرق لجمع كلّ الكتابات السينمائيّة في فضاءٍ واحدٍ،
           ولتوفير أشكالِ عرضٍ أكثر ملاءمةً لقراءةِ المقالاتِ النقديّةِ على طولِها، والنصوص عامّةً بمختلفِ أغراضِها.
           ومادام قد وقع الخيار على فضاءٍ الكترونيٍّ مُعدٍّ بالكاملِ للغرضِ، فلتكن الأحلامُ أكبر،
@@ -47,7 +47,7 @@
           وخدمةِ بحثٍ في خاصيّاتِ الفلمِ الأساسيّةِ كسنتِه (وقد اعتمدنا في ذلك على موقع قاعدة بيانات الأفلام  IMDB)
           ومخرجيه وأسمائه وبلدانه وأغراضه (Genre). إذ يمكنُ دمج بعضها أو كلّها في العمليّةِ البحثيّةِ.
         </p>
-        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf leading-none text-gray-400">
+        <p class="base-font mt-6 sm:mt-8 text-lg sm:text-xl xl:text-2xl text-center sm:text-right rtf text-gray-400">
           وما هذه إلا نسخةٌ أولى ستتبعُها تطويراتٌ جمّةٌ، وستمنح الدفترَ نفسًا أعمقَ لكتاباتٍ جديدةٍ تجعل من البحث على الموقعِ خاصيّة أشدَّ فائدةً ممّا قد تبدو عليه الآنَ.
           وفي انتظارِ تخصيص فضاءٍ لاستقبالِ المقترحاتِ، فصفحة الدّفتر على الفايسبوك ترحّبُ أيضا بمراسلاتكم. والله وليّ التوفيق.
         </p>
